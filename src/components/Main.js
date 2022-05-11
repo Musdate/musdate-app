60627c3d360b45b6a5104d25e0aedc5cb6c26cc8
Musdate <bastiantroncoso97@gmail.com>
2022-05-10 23:03:06 -0400

    [DEL] Unused var

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Grid } from '@material-ui/core';
 import { Link } from 'react-router-dom';
-import { db } from '../firebase';
 import StarRateRoundedIcon from '@material-ui/icons/StarRateRounded';
 import styled from 'styled-components';
 import Loading from './Loading';
