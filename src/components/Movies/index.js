7a6ec1c732338843dcb47a16f8c9e327ae094b49
Musdate <bastiantroncoso97@gmail.com>
2022-05-19 23:01:40 -0400

    [DEL] Unused imports

--- src/components/Movies/index.js
+++ src/components/Movies/index.js
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import styled from 'styled-components';
+// import { Link } from 'react-router-dom';
+// import styled from 'styled-components';
 import { GridContainer } from '../Globals/Grid';
 import Loading from '../Globals/Loading';
 
 function Movies(props) {
-    const [ isLoading, setIsLoading ] = useState(true);
+    const [ isLoading, ] = useState(true);
 
     return (
         <GridContainer>
